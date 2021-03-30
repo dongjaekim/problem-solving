6d46063971a66f7219cbe2506359eff3aabf30a3
dongjaekim68 <dongjaekim68@gmail.com>
2021-03-30 23:24:37 +0900

    3/28 palindromic substrings completed

--- leetcode_challenge/March/lc_210328.java
+++ leetcode_challenge/March/lc_210328.java
@@ -3,64 +3,76 @@ package March;
 public class lc_210328 {
     public static void main(String[] args) {
         Solution sol = new Solution();
+        bestSolution best_sol = new bestSolution();
 
-        int result = sol.countSubstrings("aaaaa");
+        int result = sol.countSubstrings("xabax");
         System.out.println(result);
+
+        int best_result = best_sol.countSubstrings("xabax");
+        System.out.println(best_result);
     }
 
     public static class Solution {
         public int countSubstrings(String s) {
             int output = 0;
-            boolean check = true;
+            int[][] check = new int[s.length()][s.length()];
 
-            for(int i = 0; i < s.length(); i++)
-                output += checkPalindrome(i, s.substring(i, i+1), s);
+            for(int i = s.length() - 1; i >= 0; i--) {
+                for(int j = i; j < s.length(); j++) {
+                    if(i == j)
+                        check[i][j] = 1;
 
-            for(int i = 0; i < s.length() / 2; i++) {
-                if(s.charAt(i) != s.charAt(s.length() - i - 1)) {
-                    check = false;
-                    break;
+                    if(check[i][j] == 0) {
+                        String str = s.substring(i, j+1);
+                        boolean flag = true;
+                        for(int k = 0; k < str.length() / 2; k++) {
+                            if(str.charAt(k) != str.charAt(str.length() - k - 1)) {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if(flag)
+                            check[i][j] = 1;
+                        else
+                            continue;
+                    }
+
+                    if(i > 0 && j < s.length() - 1) {
+                        if(s.charAt(i - 1) == s.charAt(j + 1))
+                            check[i - 1][j + 1] = 1;
+                    }
+                    output++;
                 }
             }
 
-            if(check)
-                output++;
-            // aaaaa - a a a a a
-            // aa aa aa aa
-            // aaa aaa aaa
-            // aaaa aaaa
-            // aaaaa
             return output;
         }
+    }
 
-        public int checkPalindrome(int start, String substr, String s) {
+    public static class bestSolution {
+        public int countSubstrings(String s) {
             int output = 0;
-            boolean check = true;
 
-            if(substr.equals(s))
-                return output;
+            if(s == null || s.length() == 0)
+                return 0;
 
-            System.out.println("start: " + start + " // substr: " + substr);
-            for(int i = 0; i < substr.length() / 2; i++) {
-                if(substr.charAt(i) != substr.charAt(substr.length() - i - 1)) {
-                    check = false;
-                    break;
-                }
-            }
-
-            if(check) {
-                output++;
-
-                if(start + substr.length() >= s.length())
-                    return output;
-
-                output += checkPalindrome(start, s.substring(start, start + substr.length() + 1), s);
-                if(start != 0 && substr.length() != s.length())
-                    output += checkPalindrome(start, s.substring(start - 1, start + substr.length() + 1), s);
+            for(int i = 0; i < s.length(); i++) {
+                output += countPalindromes(s, i, i);
+                output += countPalindromes(s, i, i+1);
             }
 
             return output;
         }
 
+        public int countPalindromes(String s, int l, int r) {
+            int cnt = 0;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                cnt++;
+                l--;
+                r++;
+            }
+
+            return cnt;
+        }
     }
 }
