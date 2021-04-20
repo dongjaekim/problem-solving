afc6aa985b9994eade627e3c67724625a95be707
dongjaekim68 <dongjaekim68@gmail.com>
2021-04-21 00:46:49 +0900

    insertion sort

---
+++ algorithm/Sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] input = {8, 9, 1, 4, 5, 2, 7, 3, 6};
+
+        input = insertionSort(input);
+        for(int i : input)
+            System.out.println(i);
+    }
+
+    public static int[] insertionSort(int[] arr) {
+        for(int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && key < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        return arr;
+    }
+}
