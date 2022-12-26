1c97e6e702615ee7f4c0186d3a296e4f483728b8
dongjaekim <dongjaekim68@gmail.com>
2022-12-26 09:45:17 +0900

    Leetcode 309. Best Time To Buy And Sell Stock With Cooldown - dp 처럼 접근해야 하는 문제 - 처음엔 dfs로 풀려고 했으나 time limit exceeded - buy, sell, cooldown에 대한 dp 배열을 따로 관리하여 푸는 방법 (maxProfit) - 위와 동일한 로직이나 배열을 쓰지 않고 변수를 이용해 O(1) space complexity로 푸는 방법 (maxProfit_best)

--- leetcode_challenge/problems/medium/P309BestTimeToBuyAndSellStockWithCooldown.java
+++ leetcode_challenge/problems/medium/P309BestTimeToBuyAndSellStockWithCooldown.java
@@ -2,8 +2,46 @@ package problems.medium;
 
 public class P309BestTimeToBuyAndSellStockWithCooldown {
 
+    public static int maxProfit_best(int[] prices) {
+        int n = prices.length;
+
+        if (n < 2)
+            return 0;
+
+        int buy = -prices[0];
+        int sell = 0;
+        int cooldown = 0;
+        for (int i = 1; i < n; i++) {
+            int prev_buy = buy;
+            int prev_sell = sell;
+            int prev_cooldown = cooldown;
+
+            buy = Math.max(prev_buy, prev_cooldown - prices[i]);
+            sell = Math.max(prev_sell, prev_buy + prices[i]);
+            cooldown = Math.max(prev_cooldown, Math.max(prev_buy, prev_sell));
+        }
+
+        return Math.max(sell, cooldown);
+    }
+
     public static int maxProfit(int[] prices) {
-        return dfs(prices, -1, 0, 0, false);
+        int n = prices.length;
+
+        if (n < 2)
+            return 0;
+
+        int[] buy = new int[n];
+        int[] sell = new int[n];
+        int[] cooldown = new int[n];
+
+        buy[0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            buy[i] = Math.max(buy[i - 1], cooldown[i - 1] - prices[i]);
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+            cooldown[i] = Math.max(cooldown[i - 1], Math.max(sell[i - 1], buy[i - 1]));
+        }
+
+        return Math.max(sell[n - 1], cooldown[n - 1]);
     }
 
     public static int dfs(int[] prices, int buy, int profit, int day, boolean isSoldYesterday) {
@@ -51,13 +89,13 @@ public class P309BestTimeToBuyAndSellStockWithCooldown {
     }
 
     public static void main(String[] args) {
-        int p1 = maxProfit(new int[]{1, 2, 3, 0, 2});
+        int p1 = maxProfit_best(new int[]{1, 2, 3, 0, 2});
         System.out.println("p1 = " + p1);
 
-        int p2 = maxProfit(new int[]{1});
+        int p2 = maxProfit_best(new int[]{1});
         System.out.println("p2 = " + p2);
 
-        int p3 = maxProfit(new int[]{1, 2, 4});
+        int p3 = maxProfit_best(new int[]{1, 2, 4});
         System.out.println("p3 = " + p3);
     }
 }
