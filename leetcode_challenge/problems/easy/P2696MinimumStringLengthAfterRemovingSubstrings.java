55275583192c4c4d56222bd8a2b7e4e695e72143
dongjaekim <dongjaekim68@gmail.com>
2024-10-07 11:17:33 +0900

    Leetcode 2696. Minimum String Length After Removing Substrings
    - 주어진 문자열에 AB나 CD가 있을때마다 지웠을 때 최소 길이
    - Stack 사용해서 구현

---
+++ leetcode_challenge/problems/easy/P2696MinimumStringLengthAfterRemovingSubstrings.java
@@ -0,0 +1,34 @@
+package problems.easy;
+
+public class P2696MinimumStringLengthAfterRemovingSubstrings {
+    public static void main(String[] args) {
+        int e1 = minLength("ABFCACDB");
+        int e2 = minLength("ACBBD");
+        int e3 = minLength("CCCCDDDD");
+
+        System.out.println("e1 = " + e1);
+        System.out.println("e2 = " + e2);
+        System.out.println("e3 = " + e3);
+    }
+
+    public static int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        char prev = '0';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((prev == 'A' && c == 'B') || (prev == 'C' && c == 'D')) {
+                stack.pop();
+                if (stack.empty())
+                    prev = '0';
+                else
+                    prev = stack.peek();
+            } else {
+                stack.push(c);
+                prev = c;
+            }
+        }
+
+        return stack.size();
+    }
+}
