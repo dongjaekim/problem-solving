70a45933d2b3c3177588ad4ba6296a86fe1f18bc
dongjaekim <dongjaekim68@gmail.com>
2024-10-10 09:35:02 +0900

    Programmers lv2 - 도넛과 막대 그래프
    - 도넛 / 막대 / 8자 그래프 및 그래프들을 이어주기 위해 생성된 한 노드가 주어졌을 때 생성된 노드 번호와 각 그래프의 수를 구하는 문제
    - 생성된 노드 구하기: outgoing edge만 있고 ingoing edge가 없음
    - 생성된 노드의 outgoing edge 수 만큼 그래프가 있다는 것을 생각할 수 있어야 함
    - 막대와 8자 그래프가 특징이 있으므로 막대와 8자 그래프를 구하고 도넛 그래프는 전체 그래프 수에서 빼서 구하기

---
+++ programmers/problems/lv2/DonutAndBarGraph.java
@@ -0,0 +1,54 @@
+package problems.lv2;
+
+import java.util.*;
+
+class DonutAndBarGraph {
+    public static void main(String[] args) {
+        int[] ints = solution(new int[][] {{4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}});
+        print(ints);
+    }
+
+    public static void print(int[] arr) {
+        System.out.print("[");
+        String collect = Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println(collect + "]");
+    }
+  
+    public static int[] solution(int[][] edges) {
+        int[] answer = new int[4];
+
+        Map<Integer, List<Integer>> inMap = new HashMap<>();
+        Map<Integer, List<Integer>> outMap = new HashMap<>();
+        Set<Integer> vertexSet = new HashSet<>();
+
+        Arrays.stream(edges).forEach(e -> {
+            outMap.computeIfAbsent(e[0], k -> new ArrayList<>());
+            outMap.get(e[0]).add(e[1]);
+
+            inMap.computeIfAbsent(e[1], k -> new ArrayList<>());
+            inMap.get(e[1]).add(e[0]);
+
+            vertexSet.add(e[0]);
+            vertexSet.add(e[1]);
+        });
+
+        outMap.entrySet().stream()
+            .filter(e -> e.getValue().size() >= 2)
+            .forEach(e -> {
+                if (inMap.get(e.getKey()) == null)
+                    answer[0] = e.getKey();
+            });
+
+        for (Integer i : vertexSet) {
+            if (i == answer[0])
+                continue;
+            if (outMap.get(i) == null)
+                answer[2]++;
+            else if(outMap.get(i).size() == 2)
+                answer[3]++;
+        }
+        answer[1] = outMap.get(answer[0]).size() - answer[2] - answer[3];
+
+        return answer;
+    }
+}
