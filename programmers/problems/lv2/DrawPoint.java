8a7877ce7f926f7b556d602af291a885a2b6ef83
dongjaekim <dongjaekim68@gmail.com>
2024-11-05 01:27:37 +0900

    Programmers lv2 - 점 찍기 - 거리에 대한 식을 생각해내면 쉽게 풀림

---
+++ programmers/problems/lv2/DrawPoint.java
@@ -0,0 +1,14 @@
+package problems.lv2;
+
+public class DrawPoint {
+    public long solution(int k, int d) {
+        long answer = 0;
+
+        for (int x = 0; x <= d; x += k) {
+            int y = (int) Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2));
+            answer += (y / k) + 1;
+        }
+
+        return answer;
+    }
+}
