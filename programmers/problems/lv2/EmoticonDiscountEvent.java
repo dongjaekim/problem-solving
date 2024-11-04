3fbc534d8e41c6d4af0e8309540923991059a07c
dongjaekim <dongjaekim68@gmail.com>
2024-11-04 23:28:52 +0900

    Programmers lv2 - 이모티콘 할인행사 - 완전탐색 알고리즘

--- programmers/problems/lv2/EmoticonDiscountEvent.java
+++ programmers/problems/lv2/EmoticonDiscountEvent.java
@@ -3,48 +3,44 @@ package problems.lv2;
 import java.util.*;
 
 class EmoticonDiscountEvent {
-    public int[] solution(int[][] users, int[] emoticons) {
-        int[] answer = new int[2];
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] discountRate = new int[emoticons.length];
-        Arrays.fill(discountRate, 10);
-        
-        findMaximumProfit(users, emoticons, discountRate, 0, map);
-        int max = 0;
-        for (int key : map.keySet()) {
-            max = Math.max(max, key);
-        }   
-        
-        answer[0] = max;
-        answer[1] = map.get(max);
-        return answer;
-    }
-    
-    public void findMaximumProfit(int[][] users, int[] emoticons, int[] discountRate, int idx, Map<Integer, Integer> map) {
-        if (discountRate[idx] > 40)
-            return;
-        
-        int emoticonPurchase = 0;
-        int emoticonServiceCount = 0;
-        for (int i = 0; i < users.length; i++) {
-            int totalPurchase = 0;
-            for (int j = 0; j < emoticons.length; j++) {
-                if (users[i][0] <= discountRate[j])
-                    totalPurchase += (int) emoticons[j] * (100 - discountRate[j]) / 100;
-            }
-            if (totalPurchase >= users[i][1])
-                emoticonServiceCount++;
-            else
-                emoticonPurchase += totalPurchase;
-        }
+    int maxServiceCount;
+    int maxPurchase;
 
-        if (map.getOrDefault(emoticonServiceCount, 0) < emoticonPurchase)
-            map.put(emoticonServiceCount, emoticonPurchase);
-        
-        for (int i = 0; i < discountRate.length; i++) {
-            discountRate[i] += 10;
-            findMaximumProfit(users, emoticons, discountRate, i, map);
-            discountRate[i] -= 10;
+    public int[] solution(int[][] users, int[] emoticons) {
+        maxServiceCount = 0;
+        maxPurchase = 0;
+
+        findMaximumProfit(users, emoticons, new int[emoticons.length], 0);
+
+        return new int[] {maxServiceCount, maxPurchase};
+    }
+
+    public void findMaximumProfit(int[][] users, int[] emoticons, int[] discountRate, int idx) {
+        if (idx == discountRate.length) {
+            int emoticonPurchase = 0;
+            int emoticonServiceCount = 0;
+            for (int i = 0; i < users.length; i++) {
+                int totalPurchase = 0;
+                for (int j = 0; j < emoticons.length; j++) {
+                    if (users[i][0] <= discountRate[j])
+                        totalPurchase += (int) emoticons[j] * (100 - discountRate[j]) / 100;
+                }
+                if (totalPurchase >= users[i][1])
+                    emoticonServiceCount++;
+                else
+                    emoticonPurchase += totalPurchase;
+            }
+
+            if (maxServiceCount < emoticonServiceCount) {
+                maxServiceCount = emoticonServiceCount;
+                maxPurchase = emoticonPurchase;
+            } else if (maxServiceCount == emoticonServiceCount)
+                maxPurchase = Math.max(maxPurchase, emoticonPurchase);
+        } else {
+            for (int i = 10; i <= 40; i += 10) {
+                discountRate[idx] = i;
+                findMaximumProfit(users, emoticons, discountRate, idx + 1);
+            }
         }
     }
 }
